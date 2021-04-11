37abbd801cc43402dc47cb7a02c108a6e670c73b
edoc99 <harshitjainf3@gmail.com>
2021-04-12 01:08:25 +0530

    feat - create resize api | Allow to resize jpg, png and bmp images based on given dimension(wd x ht)

---
+++ index.js
@@ -0,0 +1,34 @@
+// grab the packages we need
+const request = require("request").defaults({ encoding: null });
+var express = require("express");
+const resizeImg = require("resize-image-buffer");
+
+var app = express();
+var port = process.env.PORT || 5000;
+
+// routes will go here
+app.get("/api/resize", async function (req, res) {
+  const { wd, ht, url } = req.query;
+
+  await request.get(url, async (error, response, body) => {
+    console.log(response);
+    if (response.statusCode) {
+    }
+    if (!error && response.statusCode == 200) {
+      const image = await resizeImg(Buffer.from(body), {
+        width: Number(wd),
+        height: Number(ht),
+      });
+
+      res.contentType(response.headers["content-type"]);
+      return res.end(image);
+    }
+    return res.status(406).json({
+      message: "Only content of type image(bmp, jpg, png) is accepted",
+    });
+  });
+});
+
+// start the server
+app.listen(port);
+console.log("Server started! At http://localhost:" + port);
